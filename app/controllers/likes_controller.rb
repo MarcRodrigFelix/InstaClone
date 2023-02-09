3565f3cd5fffb565cdf747f0dbfc90b7e1dd64ed
marcos <m.rodrig.felix@gmail.com>
2023-02-08 21:34:02 -0800

    Add Like create method with Ajax

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+
+
+    def create
+        @like = Like.new(post_id: params[:post_id], account_id: current_account.id)
+        
+        respond_to do |format|
+            format.json {
+                if @like.save
+                    { success: true }
+                else
+                    { success: false }
+                end
+            }
+        end 
+    end
+
+
+end

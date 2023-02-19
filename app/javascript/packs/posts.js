ac241e3b19cfc700976aa28903915b64b7115799
marcos <m.rodrig.felix@gmail.com>
2023-02-18 21:20:19 -0800

    Add on click function for post comments to focus on new comment form

--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -1,22 +1,25 @@
 import $ from "jquery";
 
+$(document).on("turbolinks:load", function(){
+    // jquery loaded
 
+    $("#post-comment").on("click", function(){
+        $("#comment_comment").focus();
+    });
 
-    $(document).on("turbolinks:load", function(){
-        // jquery loaded
-        $(".post-like").on("click", function(){
-            var post_id = $(this).data("id");
-          // SEND GET REQUEST TO POST/LIKE/:POST_ID
+    $(".post-like").on("click", function(){
+        var post_id = $(this).data("id");
+        // SEND GET REQUEST TO POST/LIKE/:POST_ID
 
-           $.ajax({
-                url: "/post/like/"+post_id,
-                // url: Routes.likes_post_path(post_id),
-                method: "GET",
-           }).done(function(response){
-                console.log(response)
-            })
+        $.ajax({
+            url: "/post/like/"+post_id,
+            // url: Routes.likes_post_path(post_id),
+            method: "GET",
+        }).done(function(response){
+            console.log(response)
         });
     });
+});
 
 // $(document).ready(function() {
 // })

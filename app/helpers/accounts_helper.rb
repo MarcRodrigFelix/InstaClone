1d568b4f60a3fcc11f8248f6d6de11ce2eb2988d
marcos <m.rodrig.felix@gmail.com>
2023-02-20 21:47:09 -0800

    Add user image to comments box and link to that user

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,7 +2,7 @@ module AccountsHelper
 
     def profile_picture account, width = 100
         image_path = account.image.present? ? account.image.url : "placeholder_image.jpg"
-        image_tag(image_path, width: width, class: "img-circle")
+        image_tag(image_path, width: width, class: "img-circle m-r-5")
     end
 
     def can_edit_profile? profile_id
